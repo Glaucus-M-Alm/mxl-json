25a6bc24c6dec84e63dbdb78d66084ba33f5dbcd
Glaucus <61196676+Glaucus-M-Alm@users.noreply.github.com>
2020-12-04 14:20:05 -0300

    Add files via upload

--- test4.js
+++ test4.js
@@ -4,17 +4,20 @@ var JSZip = require("jszip");
 var arq = "./test/Abide_With_Me.mxl/";
  // read a zip file
  function convert(arq){
+     //lê o arquivo mxl
 fs.readFile(arq, function(err, data) {
     if (err) throw err;
+    //pega dentro dos conteúdos do mxl o score.xml(partitura) e o transforma em uma string
     JSZip.loadAsync(data).then(function (zip) {
       zip.file("score.xml").async("string").then(function (data) {
         //console.log("input -> %s", data)
        
-        // xml to json
+        // xml to json(pega a string e a formata no padrão JSON)
         var json = parser.toJson(data);
         //console.log("to json -> %s", json);
+        //Nome do arquivo desejado
         var outputFilename = './test/Score.json/';
-
+//cria o arquivo com o nome desejado JSON.stringfy torna o JSON bonitinho
 fs.writeFile(outputFilename, JSON.stringify(json, null, 4), function(err) {
     if(err) {
       console.log(err);
