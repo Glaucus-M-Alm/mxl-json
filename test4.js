98ef5eb6cfaf2a36a3bbf19b2c9e4fd01ed09f12
Glaucus <61196676+Glaucus-M-Alm@users.noreply.github.com>
2020-12-04 13:41:58 -0300

    Add files via upload

---
+++ test4.js
@@ -0,0 +1,38 @@
+var parser = require('xml2json');
+var fs = require("fs");
+var JSZip = require("jszip");
+var arq = "./test/Abide_With_Me.mxl/";
+ // read a zip file
+ function convert(arq){
+fs.readFile(arq, function(err, data) {
+    if (err) throw err;
+    JSZip.loadAsync(data).then(function (zip) {
+      zip.file("score.xml").async("string").then(function (data) {
+        //console.log("input -> %s", data)
+       
+        // xml to json
+        var json = parser.toJson(data);
+        //console.log("to json -> %s", json);
+        var outputFilename = './test/Score.json/';
+
+fs.writeFile(outputFilename, JSON.stringify(json, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + outputFilename);
+    }
+}); 
+       /*fs.writeFile('test.json', json, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          });*/
+        // json to xml
+       /* var xml = parser.toXml(json);
+        console.log("back to xml -> %s", xml)*/
+      });
+      
+    });
+});
+return 0;
+ };
+convert(arq);
