5dfc2960eeccf10fc709bdab43260e16ad6b9b64
Glaucus <61196676+Glaucus-M-Alm@users.noreply.github.com>
2020-12-07 12:47:39 -0300

    Add files via upload

--- test5.js
+++ test5.js
@@ -2,9 +2,9 @@ var parser = require('xml2json');
 var fs = require("fs");
 var JSZip = require("jszip");
 var arq = "./test/Abide_With_Me.mxl/";
- function convert(arq){
+ function convert(arqu){
      //lê o arquivo mxl(se fosse um zip também funcionaria)arq TEM que ser o caminho do arquivo
-fs.readFile(arq, function(err, data) {
+fs.readFile(arqu, function(err, data) {
     if (err) throw err;
     //pega dentro dos conteúdos do mxl o score.xml(partitura) e o transforma em uma string
     JSZip.loadAsync(data).then(function (zip) {
